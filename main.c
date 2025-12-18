891e82b72b156936d61c2c8c6a4f6eef9b2aa9cf
KWAN PAW <liqianzhan666@gmail.com>
2025-12-18 16:42:25 +0800

    Initial commit: Add main and header structure

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello Matrix Project!\n");
+    return 0;
+}
---
+++ matrix_op.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_OP_H
+#define MATRIX_OP_H
+
+#define SIZE 3
+
+// functions declare
+
+#endif
